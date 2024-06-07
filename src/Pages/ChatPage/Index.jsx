08fa948cc8317f38f58c46e6babe4dc03545e3f9
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-06-07 10:25:09 +0330

    update chat page
    create modal for create new group and create new channel

--- src/Pages/ChatPage/Index.jsx
+++ src/Pages/ChatPage/Index.jsx
@@ -15,7 +15,7 @@ import {
   Modal,
   Stack,
   TextField,
-  Typography,
+  Typography
 } from "@mui/material";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
@@ -26,23 +26,23 @@ import React, { useRef, useState } from "react";
 import ChatCard from "../../Components/ChatCard/Index";
 import GroupsIcon from "@mui/icons-material/Groups";
 import SettingsIcon from "@mui/icons-material/Settings";
-<<<<<<< Updated upstream
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import SendIcon from '@mui/icons-material/Send';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
-import AttachmentIcon from '@mui/icons-material/Attachment';
-=======
+import SendIcon from "@mui/icons-material/Send";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import AttachmentIcon from "@mui/icons-material/Attachment";
 import ContactCard from "../../Components/ContactCard";
->>>>>>> Stashed changes
 
 const ChatPage = () => {
   const inputRef = useRef(null);
   const [img, setImg] = useState();
 
   const [openDrawer, setOpenDrawer] = React.useState(false);
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [openGroup, setOpenGroup] = React.useState(false);
+  const [openChannel, setOpenChannel] = React.useState(false);
+  const handleOpenGroup = () => setOpenGroup(true);
+  const handleCloseGroup = () => setOpenGroup(false);
+  const handleOpenChannel = () => setOpenChannel(true);
+  const handleCloseChannel = () => setOpenChannel(false);
   const handleClick = () => {
     inputRef.current.click();
   };
@@ -55,12 +55,7 @@ const ChatPage = () => {
   };
   return (
     <>
-      <Stack
-        flexDirection={"row"}
-        width={"100%"}
-        height={"100vh"}
-        bgcolor={""}
-      >
+      <Stack flexDirection={"row"} width={"100%"} height={"100vh"} bgcolor={""}>
         <Stack
           width={"25%"}
           height={"100%"}
@@ -86,22 +81,22 @@ const ChatPage = () => {
                 borderRadius: "24px",
                 "& .MuiOutlinedInput-root": {
                   "& fieldset": {
-                    border: "none",
+                    border: "none"
                   },
                   "&:hover fieldset": {
-                    border: "none",
+                    border: "none"
                   },
                   "&.Mui-focused fieldset": {
-                    border: "none",
-                  },
-                },
+                    border: "none"
+                  }
+                }
               }}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
                     <SearchIcon />
                   </InputAdornment>
-                ),
+                )
               }}
               placeholder="search"
             />
@@ -127,7 +122,13 @@ const ChatPage = () => {
             </List>
           </Stack>
         </Stack>
-        <Stack direction={'column'} justifyContent={'space-between'} borderLeft={"1px #cccccc solid"} width={"75%"} height={"100%"}>
+        <Stack
+          direction={"column"}
+          justifyContent={"space-between"}
+          borderLeft={"1px #cccccc solid"}
+          width={"75%"}
+          height={"100%"}
+        >
           <Stack direction={"column"}>
             <Stack
               width={"100%"}
@@ -151,7 +152,8 @@ const ChatPage = () => {
           </Stack>
           <Stack
             sx={{
-              backgroundImage: "url(https://w0.peakpx.com/wallpaper/425/514/HD-wallpaper-telegram-pattern-art-patterns.jpg)",
+              backgroundImage:
+                "url(https://w0.peakpx.com/wallpaper/425/514/HD-wallpaper-telegram-pattern-art-patterns.jpg)",
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
@@ -161,15 +163,18 @@ const ChatPage = () => {
           >
             {/* اینجا محتوای استک قرار می‌گیرد */}
           </Stack>
-          <Stack direction={'row-reverse'} height={'60px'} width={'100%'} >
+          <Stack direction={"row-reverse"} height={"60px"} width={"100%"}>
             <Button>
-            <SendIcon/>
+              <SendIcon />
             </Button>
             <Button>
-            <SentimentSatisfiedAltIcon style={{color:"gray"}}/>
+              <SentimentSatisfiedAltIcon style={{ color: "gray" }} />
             </Button>
-            
-            <TextField fullWidth placeholder="Write a message..." sx={{
+
+            <TextField
+              fullWidth
+              placeholder="Write a message..."
+              sx={{
                 "& .MuiOutlinedInput-root": {
                   "& fieldset": {
                     border: "none"
@@ -181,24 +186,16 @@ const ChatPage = () => {
                     border: "none"
                   }
                 }
-            }}/>
-            <Button sx={{padding:'0'}}>
-            <AttachmentIcon style={{color:"gray"}}/>
+              }}
+            />
+            <Button sx={{ padding: "0" }}>
+              <AttachmentIcon style={{ color: "gray" }} />
             </Button>
           </Stack>
         </Stack>
       </Stack>
-<<<<<<< Updated upstream
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        <Box
-          sx={{ width: 300 }}
-          role="presentation"
-          onClick={toggleDrawer(false)}
-        >
-=======
       <Drawer open={openDrawer} onClose={toggleDrawer(false)}>
         <Box sx={{ width: 300 }} role="presentation">
->>>>>>> Stashed changes
           <Stack
             flexDirection={"row"}
             height={"100px"}
@@ -212,22 +209,135 @@ const ChatPage = () => {
           <Divider variant="fullWidth" />
           <List disablePadding>
             <ListItem disablePadding>
-<<<<<<< Updated upstream
-              <ListItemButton>
-=======
-              <ListItemButton onClick={handleOpen}>
->>>>>>> Stashed changes
+              <ListItemButton onClick={handleOpenGroup}>
                 <ListItemIcon>
                   <GroupsIcon />
                 </ListItemIcon>
                 <ListItemText primary={"New Group"} />
               </ListItemButton>
             </ListItem>
-<<<<<<< Updated upstream
-=======
             <Modal
-              open={open}
-              onClose={handleClose}
+              open={openGroup}
+              onClose={handleCloseGroup}
+              
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  width: 350,
+                  bgcolor: "background.paper",
+                  border: "2px solid white",
+                  borderRadius: "16px",
+                  boxShadow: 24,
+                  p: 4,
+                  display: "flex",
+                  flexDirection: "column"
+                }}
+              >
+                <Stack
+                  alignItems={"center"}
+                  flexDirection={"row"}
+                  justifyContent={"space-between"}
+                >
+                  <Stack
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                    sx={{
+                      height: "70px",
+                      width: "70px",
+                      borderRadius: "50%",
+                      bgcolor: "#39a5db"
+                    }}
+                  >
+                    <IconButton onClick={handleClick}>
+                      <CameraAltIcon
+                        sx={{ color: "#fff", fontSize: "2.5rem" }}
+                      />
+                    </IconButton>
+                    <Input
+                      inputRef={inputRef}
+                      sx={{
+                        display: "none"
+                      }}
+                      type="file"
+                      onChange={handleImageChange}
+                    />
+                  </Stack>
+                  <Stack>
+                    <TextField
+                      id="standard-basic"
+                      label="Group Name"
+                      variant="standard"
+                    />
+                  </Stack>
+                </Stack>
+                <Stack mt={2}>
+                  <TextField
+                    size="small"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <SearchIcon />
+                        </InputAdornment>
+                      )
+                    }}
+                    sx={{
+                      backgroundColor: "#F1F1F1",
+                      borderRadius: "24px",
+                      "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                          border: "none"
+                        },
+                        "&:hover fieldset": {
+                          border: "none"
+                        },
+                        "&.Mui-focused fieldset": {
+                          border: "none"
+                        }
+                      }
+                    }}
+                  />
+                  <Stack
+                    sx={{
+                      backgroundColor: "#F1F1F1",
+                      borderRadius: "16px",
+                      maxHeight: "250px",
+                      overflowY: "scroll"
+                    }}
+                    mt={1}
+                  >
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                  </Stack>
+                </Stack>
+                <Stack mt={1} >
+                  <Button variant="contained">
+                    Create
+                  </Button>
+                </Stack>
+              </Box>
+            </Modal>
+            <ListItem disablePadding>
+              <ListItemButton onClick={handleOpenChannel}>
+                <ListItemIcon>
+                  <ForumIcon />
+                </ListItemIcon>
+                <ListItemText primary={"New Channel"} />
+              </ListItemButton>
+            </ListItem>
+            <Modal
+              open={openChannel}
+              onClose={handleCloseChannel}
               aria-labelledby="modal-modal-title"
               aria-describedby="modal-modal-description"
             >
@@ -244,7 +354,7 @@ const ChatPage = () => {
                   boxShadow: 24,
                   p: 4,
                   display: "flex",
-                  flexDirection: "column",
+                  flexDirection: "column"
                 }}
               >
                 <Stack
@@ -259,7 +369,7 @@ const ChatPage = () => {
                       height: "70px",
                       width: "70px",
                       borderRadius: "50%",
-                      bgcolor: "#39a5db",
+                      bgcolor: "#39a5db"
                     }}
                   >
                     <IconButton onClick={handleClick}>
@@ -270,7 +380,7 @@ const ChatPage = () => {
                     <Input
                       inputRef={inputRef}
                       sx={{
-                        display: "none",
+                        display: "none"
                       }}
                       type="file"
                       onChange={handleImageChange}
@@ -279,25 +389,72 @@ const ChatPage = () => {
                   <Stack>
                     <TextField
                       id="standard-basic"
-                      label="Group Name"
+                      label="Channel Name"
                       variant="standard"
                     />
                   </Stack>
+                  
                 </Stack>
-                <Stack mt={2}>
-                  <ContactCard />
+                <Stack mt={1} gap={1}>
+                <TextField
+                      id="standard-basic"
+                      label="Channel Id"
+                      variant="standard"
+                      size="small"
+
+                    />
+                  <TextField
+                    size="small"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <SearchIcon />
+                        </InputAdornment>
+                      )
+                    }}
+                    sx={{
+                      backgroundColor: "#F1F1F1",
+                      borderRadius: "24px",
+                      "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                          border: "none"
+                        },
+                        "&:hover fieldset": {
+                          border: "none"
+                        },
+                        "&.Mui-focused fieldset": {
+                          border: "none"
+                        }
+                      }
+                    }}
+                  />
+                  <Stack
+                    sx={{
+                      backgroundColor: "#F1F1F1",
+                      borderRadius: "16px",
+                      maxHeight: "250px",
+                      overflowY: "scroll"
+                    }}
+                    mt={1}
+                  >
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                    <ContactCard/>
+                  </Stack>
+                </Stack>
+                <Stack mt={1} >
+                  <Button variant="contained">
+                    Create
+                  </Button>
                 </Stack>
               </Box>
             </Modal>
->>>>>>> Stashed changes
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <ForumIcon />
-                </ListItemIcon>
-                <ListItemText primary={"New Channel"} />
-              </ListItemButton>
-            </ListItem>
             <ListItem disablePadding>
               <ListItemButton>
                 <ListItemIcon>
